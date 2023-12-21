f027bb804e34db221ea912a147eb931354436b68
nicolas girard <ngirard44@yahoo.fr>
2023-12-21 23:32:26 +0100

    add task  new features as duetime then code into task api in data/service/taskapi

--- app/src/main/java/com/openclassrooms/myrepo/model/Task.java
+++ app/src/main/java/com/openclassrooms/myrepo/model/Task.java
@@ -1,61 +1,86 @@
 package com.openclassrooms.myrepo.model;
 
+import java.util.Date;
 import java.util.Objects;
 
 /**
- * Une classe représentant une tâche avec une description.
+ * class représentant une tâche avec sa description et sa date d'échéance
+ *
  */
 public class Task {
     private String description;
 
-    /**
-     * Constructeur pour créer une nouvelle tâche avec une description.
-     *
-     * @param description La description de la tâche.
-     */
-    public Task(String description) {
-        this.description = description;
-    }
+    private  Date dueTime;
+
 
     /**
-     * Obtient la description de la tâche.
      *
-     * @return La description de la tâche.
+     * @param description description de la tâche
+     * @param dueTime date d'échéance de la tâche
      */
+
+    public Task(String description, Date dueTime) {
+        this.description = description;
+        this.dueTime = dueTime;
+}
+
+
+    /**
+     *
+     * @return date d'échéance de la tâche
+     *
+     *
+     *
+     */
+
+
+
+    public Date getDueTime(){ return dueTime;}
+
+    /**
+     *
+     * @return description de la tâche
+     */
+
     public String getDescription() {
         return description;
     }
 
     /**
-     * Modifie la description de la tâche.
      *
-     * @param description La nouvelle description de la tâche.
+     * @param description nouvelle desciption de la tâche
      */
     public void setDescription(String description) {
         this.description = description;
     }
 
     /**
-     * Vérifie si deux objets Task sont égaux en comparant leurs descriptions.
      *
-     * @param o L'objet à comparer.
-     * @return Vrai si les descriptions sont égales, sinon faux.
+     * @param dueTime nouvelle date d'échéance
      */
+    public void setDueTime(Date dueTime){this.dueTime = dueTime;}
+
+    /**
+     * vérifie si les deux objet task sont égaux en comparant leurs descriptions et leurs dates d'échéance
+     * @param o l'objet à comparer
+     * @return vrai si la description et les dates d'échéace sont égales, c'est faux sinon.
+     */
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return Objects.equals(description, task.description);
+        return Objects.equals(description, task.description) && Objects.equals(dueTime, task.dueTime);
     }
 
+
     /**
-     * Calcule le code de hachage en utilisant la description de la tâche.
-     *
-     * @return Le code de hachage calculé.
+     * Calcule le code de hachage en utilisant la description et la date d'échéance de la tâche.
+     * @return le code de hachage est calculé.
      */
     @Override
     public int hashCode() {
-        return Objects.hash(description);
+        return Objects.hash(description, dueTime);
     }
 }
